4ba19b41fb94adae4a42cb01677fb2b3f052cfcb
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2021-05-01 23:11:21 -0400

    Add files via upload

---
+++ JavaProgram1/src/JavaProgram1.java
@@ -0,0 +1,8 @@
+public class JavaProgram1 {
+    public static void main(String[] args){
+        System.out.println("This is a sentence printed using the println method");
+        System.out.print("This is a sentence printed using the print method");
+        System.out.printf("\nThis is the %drd sentence printed using the printf method",3);
+    }
+}
+`
